1fb9bf0b94c979a6b7bd9ea12cdfedbb3697084c
Scott D. Rothrock <srothroc@gmail.com>
2012-02-07 22:39:44 +0900

    Renamed the wrong file -- uploading correct recursive_generator_sieve.py now.

--- old_algorithms/recursive_generator_sieve.py
+++ old_algorithms/recursive_generator_sieve.py
@@ -1,78 +1,57 @@
-import math
-from PySide.QtCore import *
-from PySide.QtGui import *
+# Hits the recursion limit.
 
-# Prime Number Sieve using a loop by Scott Rothrock
+def x_primes(x, prime_list):
+    for i in xrange(x):
+        yield prime_list.next()
 
-__prime_master_list__ = []
+def initial_range(lower_bound):
+    while True:
+        yield lower_bound
+        lower_bound = lower_bound + 1
 
-def is_prime(input_number):
-    for index in xrange(0, len(__prime_master_list__)):
-        if input_number % __prime_master_list__[index] == 0:
-            return False
+def primes_in_range(lower_bound, upper_bound, prime_list):
+    while True:
+        curr_prime = prime_list.next()
+        
+        if curr_prime > upper_bound:
+            return
+        else:
+            yield curr_prime
+
+def sieve(int_range, curr_num):
+    for i in int_range:
+        if i % curr_num != 0:
+            yield i
+
+def make_prime_list(int_range):
+    while True:
+        prime = int_range.next()
+        yield prime
+        int_range = sieve(int_range, prime)
+
+def find_x_primes(x, lower_bound = 2):
+    prime_list = []
     
-        if __prime_master_list__[index] > math.sqrt(input_number * 1.0):
-            __prime_master_list__.append(input_number)
-            return True
-
-def find_primes_in_range(lower_bound, upper_bound, primes_found, progress_bar = None):
-    curr_progress = 0
-    past_progress = 0
+    if lower_bound == 1:
+        prime_list.append(1)
+        lower_bound = 2
     
-    for input_number in xrange(lower_bound, upper_bound + 1, 2):
+    for i in x_primes(x, make_prime_list(initial_range(lower_bound))):
+        prime_list.append(i)
+    
+    return prime_list
+
+def find_primes_in_range(lower_bound, upper_bound, progress_bar = None):
+    prime_list = []
+    
+    if lower_bound == 1:
+        prime_list.append(1)
+        lower_bound = 2
+    
+    for i in primes_in_range(lower_bound, upper_bound, make_prime_list(initial_range(lower_bound))):
+        prime_list.append(i)
+        
         if progress_bar != None:
-            curr_progress = int(100 * ((input_number - lower_bound) * 1.0 / (upper_bound - lower_bound)))
-            
-            if curr_progress > past_progress + 4:
-                progress_bar.setValue(curr_progress)            
-                past_progress = curr_progress
-            
-        if is_prime(input_number) and lower_bound <= input_number <= upper_bound:                
-            primes_found.append(input_number)
+            progress_bar.setValue(i)
     
-    # In the event that the upper bound is not prime,
-    # we still want to push the progress bar to 100%.
-    if progress_bar != None:
-        progress_bar.setValue(100)
-    
-    return primes_found
-
-def search_range(lower_bound, upper_bound, progress_bar = None, thread_storage = None):
-    """
-    search_range(lower_bound, upper_bound) finds primes in the range lower_bound to upper_bound and then returns a list.
-    Both inputs must be positive integers.
-    """
-    
-    primes_found = []
-    
-    if len(__prime_master_list__) == 0:
-        __prime_master_list__.append(2)
-    
-    # 1 and 2 are special cases.
-    # We know that they are basic primes.
-    if lower_bound <= 2:
-        if lower_bound == 1:
-            primes_found.append(1)
-            
-        primes_found.append(2)
-        lower_bound = 3
-    
-    #if thread_storage != None:
-        #thread_storage.put_nowait(find_primes_in_range(lower_bound, upper_bound, primes_found, progress_bar))
-    #else:
-    return find_primes_in_range(lower_bound, upper_bound, primes_found, progress_bar)
-
-# This isn't the fastest way, but it is the simplest for our purposes.
-def find_single(input_number):
-    """
-    find_single(input_number) returns True if input_number is a prime.
-    input_number must be a positive integer.
-    """
-    
-    if __prime_master_list__.count(input_number) > 0:
-        return True
-    
-    if len(search_range(input_number, input_number)) == 0:
-        return False
-    else:
-        return True
+    return prime_list
